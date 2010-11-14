5cc9b16b4bdbdfdaa5aaf7c22c76b11a9bb45d50
Kamil Giszczak <beerkg@gmail.com>
2010-11-14 16:37:31 +0100

    Added worker

---
+++ lib/crom/worker.rb
@@ -0,0 +1,44 @@
+module Crom
+  class Worker
+    attr_accessor :logger
+
+    def initialize
+      @scheduler = Rufus::Scheduler.start_new
+      instance_eval(&Crom.tasks)
+
+      trap("TERM") { stop }
+      trap("INT")  { stop }
+    end
+
+    def start
+      log "Crom Worker started at #{Time.now.strftime('%F %T')}"
+      @scheduler.join
+    end
+
+    def stop
+      log "Crom Worker stopped at #{Time.now.strftime('%F %T')}"
+      @scheduler.stop
+    end
+
+    private
+      [ :schedule_in, :at, :every, :cron ].each do |method|
+        class_eval <<-ENDEVAL
+          def #{method}(t, s = nil, opts = {}, &block)
+            decorated_block = Proc.new do
+              log "\#{Time.now.strftime('%F %T')} | Processing task: #{method} \#{t}"
+              begin
+                block.call
+              rescue
+                log $!
+              end
+            end
+            @scheduler.#{method}(t, s, opts, &decorated_block)
+          end
+        ENDEVAL
+      end
+
+      def log(msg)
+        logger.info msg if logger
+      end
+  end # class Worker
+end # module Crom

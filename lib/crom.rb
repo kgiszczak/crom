dcf74c289406a06cb4494feab311523188183187
Kamil Giszczak <beerkg@gmail.com>
2010-11-14 16:43:40 +0100

    Implemented base functionality

--- lib/crom.rb
+++ lib/crom.rb
@@ -1,2 +1,15 @@
+require 'rufus/scheduler'
+
+require File.dirname(__FILE__) + '/crom/worker'
+require File.dirname(__FILE__) + '/crom/version'
+require File.dirname(__FILE__) + '/crom/railtie'
+
 module Crom
+  def self.schedule(&block)
+    @tasks = block
+  end
+
+  def self.tasks
+    @tasks || Proc.new {}
+  end
 end

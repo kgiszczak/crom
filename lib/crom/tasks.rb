84cb9d0f121543fa570dcd9037268a10b4eba693
Kamil Giszczak <beerkg@gmail.com>
2010-11-14 18:28:03 +0100

    Renamed start task to run

--- lib/crom/tasks.rb
+++ lib/crom/tasks.rb
@@ -1,6 +1,6 @@
 namespace :crom do
   desc "Start a crom worker."
-  task :start => :environment do
+  task :run => :environment do
     load Rails.root + 'config/schedule.rb'
     Crom::Worker.logger = Logger.new($stdout)
     Crom::Worker.new.start

59b558a3304593e0e7adbc6163a83076476b2758
Kamil Giszczak <beerkg@gmail.com>
2010-11-15 17:25:54 +0100

    Require railtie only when Rails is available

--- lib/crom.rb
+++ lib/crom.rb
@@ -2,7 +2,7 @@ require 'rufus/scheduler'
 
 require File.dirname(__FILE__) + '/crom/worker'
 require File.dirname(__FILE__) + '/crom/version'
-require File.dirname(__FILE__) + '/crom/railtie'
+require File.dirname(__FILE__) + '/crom/railtie' if defined?(Rails)
 
 module Crom
   def self.schedule(&block)

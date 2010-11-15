fbb4cbd7543e69fd877a2b3c6c9c4e606e8ec631
Kamil Giszczak <beerkg@gmail.com>
2010-11-15 18:11:29 +0100

    Added more examples to template schedule file

--- lib/generators/crom/templates/schedule.rb
+++ lib/generators/crom/templates/schedule.rb
@@ -4,4 +4,16 @@ Crom.schedule do
     # Some task
   # end
 
+  # cron '* * * * *' do
+    # Some task
+  # end
+
+  # schedule_in '20m' do
+    # Some task
+  # end
+
+  # at 'Thu Mar 26 07:31:43 +0900 2020' do
+    # Some task
+  # end
+
 end
